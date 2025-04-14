07ca4949c1c1057b1e60777d7f5d70fe4129feaf
Shaun Fulton <fulton_shaun@yahoo.com>
2025-04-13 21:09:30 -0400

    questions and answer list created

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] questions = {
+    "What is 5 + 3?",
+    "What is the capital of France?",
+    "What color is the sky on a clear day?"
+};
+
+string[] answers = {
+    "8",
+    "Paris",
+    "Blue"
+};
